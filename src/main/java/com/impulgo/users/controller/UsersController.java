d5315490ed81ac2b694157314157e7bf76fb09bb
Jayanta <Jayanta@DESKTOP-0C8PP6M>
2020-11-02 01:12:56 +0530

    removed unwanted imports

--- src/main/java/com/impulgo/users/controller/UsersController.java
+++ src/main/java/com/impulgo/users/controller/UsersController.java
@@ -3,8 +3,6 @@ package com.impulgo.users.controller;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
